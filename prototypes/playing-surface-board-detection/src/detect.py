f107f8e760ab5c3265de5c2a8644579456566720
Likkan Chung <llc721@student.bham.ac.uk>
2021-11-10 22:01:17 +0000

    Add bound drawing on board

--- prototypes/playing-surface-board-detection/src/detect.py
+++ prototypes/playing-surface-board-detection/src/detect.py
@@ -1,7 +1,47 @@
 import cv2
+import json
 
 def find_anchor_points(image):
     qr_code_detector = cv2.QRCodeDetector()
-    codes = qr_code_detector.detectAndDecodeMulti(image)
-    print(codes)
+    _, data_tuple, points_tuple, _ = qr_code_detector.detectAndDecodeMulti(image)
 
+    tags = []
+
+    # Match up points to anchors
+    if len(data_tuple) == len(points_tuple):
+        data = list(data_tuple)
+        points = list(points_tuple)
+
+        for index in range(len(data)):
+            tag = json.loads(data[index])
+            tag['points'] = points[index].tolist()
+            tags.append(tag)
+
+    return tags
+
+
+def tag_board(tags):
+    board_tags = {}
+    for tag in tags:
+        print(tag)
+        if tag['id'] == 'board':
+            board_tags[tag['anchor']] = _get_center(tag['points'])
+    return board_tags
+
+
+def _get_center(points):
+    # A simple approach but does not account for parallax
+    x = [i[0] for i in points]
+    y = [i[1] for i in points]
+    mid_x = int((min(x) + max(x)) / 2)
+    mid_y = int((min(y) + max(y)) / 2)
+    return [mid_x, mid_y]
+
+
+def draw_bounds(image, points):
+    num_points = len(points)
+    for i in range(num_points):
+        point_1 = points[i]
+        point_2 = points[(i+1) % num_points]
+        cv2.line(image, point_1, point_2, color=(255, 0, 0), thickness=2)
+    return image
--- prototypes/playing-surface-board-detection/tests/test_detect.py
+++ prototypes/playing-surface-board-detection/tests/test_detect.py
@@ -1,9 +1,17 @@
-from detect import find_anchor_points
+from detect import find_anchor_points, tag_board, draw_bounds
 import cv2
 
 
 def test_detect(mock_board):
-    find_anchor_points(mock_board)
+    tags = find_anchor_points(mock_board)
+    board_tags = tag_board(tags)
+    image = draw_bounds(mock_board, list(board_tags.values()))
+    h, w, _ = mock_board.shape
+    image_scaled = cv2.resize(image, (int(w/2.5), int(h/2.5)))
+    cv2.imshow("img", image_scaled)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+
 
 
 if __name__ == '__main__':
